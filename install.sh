2b408da2bb4d4c326fb0e0d93efd3b27c3e47e45
JwpAT <150574239+JwpAT@users.noreply.github.com>
2024-12-23 22:00:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
-if command -v yay &> /dev/null; then
-	echo "yay is already installed."
-else
-	echo "yay and sddm are not installed. Installing..."
-	sudo pacman -S --needed git base-devel
-	git clone https://aur.archlinux.org/yay.git
-	cd yay
-	makepkg -si
-	cd ..
-	rm -rf yay
- 	sudo pacman -S sddm
-  	systemctl enable sddm
-	echo "yay and sddm have been installed successfully."
-fi
+echo "yay and sddm are not installed. Installing..."
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+sudo pacman -S sddm
+systemctl enable sddm
+echo "yay and sddm have been installed successfully."
+
+mkdir ~/.themes
+
 echo "installing requirement"
 yay -S --needed - < requirements
+
+echo "Extracting Kripton theme..."
+tar -xf ~/hyprland1/Kripton.tar.xz
+mv ~/Kripton ~/.themes/
+
+echo "Kripton theme has been installed successfully."
+
+echo "Installation has completed. Reboot your fucking system and don't break it, or else you'll have to deal with me again. Have fun faggot."
