d59db3b0b2bdb06ef3a0368e7c2a5e55451d14a6
JwpAT <150574239+JwpAT@users.noreply.github.com>
2024-12-23 21:22:31 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 if command -v yay &> /dev/null; then
 	echo "yay is already installed."
 else
-	echo "yay is not installed. Installing..."
+	echo "yay and sddm are not installed. Installing..."
 	sudo pacman -S --needed git base-devel
 	git clone https://aur.archlinux.org/yay.git
 	cd yay
 	makepkg -si
 	cd ..
 	rm -rf yay
-	echo "yay has been installed successfully."
+ 	sudo pacman -S sddm
+  	systemctl enable sddm
+	echo "yay and sddm have been installed successfully."
 fi
 echo "installing requirement"
 yay -S --needed - < requirements
