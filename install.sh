4196d33978e578d40610c3dec3847c437eddd664
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-01-27 22:25:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,14 +16,13 @@ echo "Installing Catppuccin Mocha theme..."
 mkdir ~/.themes
 unzip ~/hypr/Catppuccin-Mocha-Standard-Blue-Dark.zip
 cp -r ~/hypr/Catppuccin-Mocha-Standard-Blue-Dark ~/.themes/
-
 echo "Catppuccin Mocha theme has been installed successfully."
 
 curl -fsSL https://ollama.com/install.sh | sh
 
 echo "Installing config files"
 mv ~/hypr/config/* ~/.config/
-
 chmod +x ~/.config/hypr/hyprlock-greeter.sh
+sudo cp -r ~/.config/wlogout/icons/ /usr/share/wlogout/
 
 echo "Installation has completed. Reboot your system. Have fun!!"
