96bfb1c6b6531616762d6c38a23fcb36333bea6b
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-03-15 12:26:53 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,4 +1,4 @@
-echo "yay and sddm are not installed. Installing..."
+echo "istalling yay and sddm"
 sudo -i pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -6,21 +6,22 @@ makepkg -si
 cd ..
 rm -rf yay
 pacman -S sddm
-systemctl enable sddm
+sudo systemctl enable sddm
 echo "yay and sddm have been installed successfully."
 
 echo "installing packages"
 yay -S --needed - < packages
 
-echo "Installing Catppuccin Mocha theme..."
+echo "installing Catppuccin Mocha theme..."
 mkdir ~/.themes
 unzip ~/hypr/Catppuccin-Mocha-Standard-Blue-Dark.zip
 cp -r ~/hypr/Catppuccin-Mocha-Standard-Blue-Dark ~/.themes/
 echo "Catppuccin Mocha theme has been installed successfully."
 
+echo "downloading Ollama3 local LLM"
 curl -fsSL https://ollama.com/install.sh | sh
 
-echo "Installing config files"
+echo "installing config files"
 mv ~/hypr/config/* ~/.config/
 chmod +x ~/.config/hypr/hyprlock-greeter.sh
 cp -r ~/.config/wlogout/icons/ /usr/share/wlogout/
