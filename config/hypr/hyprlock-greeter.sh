9b3a06faee7db84f9407ac8450a248b9039223d8
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-04-04 21:09:16 -0400

    Delete config/hypr/hyprlock-greeter.sh

--- config/hypr/hyprlock-greeter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-hour=$(date +"%H")
-if [ "$hour" -ge 5 ] && [ "$hour" -lt 12 ]; then
-  echo "good morning, $USER"
-elif [ "$hour" -ge 12 ] && [ "$hour" -lt 17 ]; then
-  echo "good afternoon, $USER"
-else
-  echo "good evening, $USER"
-fi
