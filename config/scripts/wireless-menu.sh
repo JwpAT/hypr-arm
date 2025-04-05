8427a0c31a1b4d0aa132fda19319f113a46cb80e
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-04-04 21:12:39 -0400

    Add files via upload

---
+++ config/scripts/wireless-menu.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+export DIALOGRC="$HOME/.config/dilog/dialogrc"
+export TERM=xterm-old
+unset COLORTERM
+
+while true; do
+    CHOICE=$(dialog --clear --stdout \
+        --title "Control Center" \
+        --menu "Select an option:" 10 50 3 \
+        1 "Network Manager (nmtui)" \
+        2 "Bluetooth Control (bluetoothctl)" \
+        3 "Exit")
+
+    case "$CHOICE" in
+        1)
+            TERM=xterm-old nmtui
+            ;;
+        2)
+            bluetoothctl
+            ;;
+        3)
+            clear
+            break
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
