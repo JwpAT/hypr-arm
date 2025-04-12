df75a6ff7e8366d2be7b2cf773c3e2c8883a6b70
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-04-12 17:52:21 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,6 +23,26 @@ systemctl start bluetooth.service
 
 curl -fsSL https://ollama.com/install.sh | sh
 
+# Prompt to choose LLM
+echo "Which LLM would you like to install?"
+echo "1) llama3.2:1b (Smaller, Faster)"
+echo "2) llama3.2 (Bigger, Slower"
+read -p "Enter the number of your choice: " choice
+
+case $choice in
+  1)
+    echo "Downloading llama3.2:1b..."
+    ollama pull llama3.2:1b
+    ;;
+  2)
+    echo "Downloading llama3.2..."
+    ollama pull llama3.2
+    ;;
+  *)
+    echo "Invalid choice. Skipping LLM download."
+    ;;
+esac
+
 echo "Installing config files"
 mv ~/hypr/config/* ~/.config/
 chmod +x ~/.config/scripts/hyprlock-greeter.sh
