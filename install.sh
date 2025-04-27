8db73b314f64ffc07aac0aff645a95c7f80bfc97
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-04-27 15:08:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,22 +21,26 @@ echo "Catppuccin Mocha theme has been installed successfully."
 systemctl enable bluetooth.service
 systemctl start bluetooth.service 
 
-curl -fsSL https://ollama.com/install.sh | sh
-
 # Prompt to choose LLM
-echo "Which LLM would you like to install?"
+echo "Would you like the install an LLM?"
 echo "1) llama3.2:1b (Smaller, Faster)"
-echo "2) llama3.2 (Bigger, Slower"
+echo "2) llama3.2 (Larger, Slower)"
+echo "3) skip LLM installation
 read -p "Enter the number of your choice: " choice
 
 case $choice in
   1)
     echo "Downloading llama3.2:1b..."
+    curl -fsSL https://ollama.com/install.sh | sh
     ollama pull llama3.2:1b
     ;;
   2)
     echo "Downloading llama3.2..."
+    curl -fsSL https://ollama.com/install.sh | sh
     ollama pull llama3.2
+    ;;
+  3)
+    echo "Skipping LLM download"
     ;;
   *)
     echo "Invalid choice. Skipping LLM download."
