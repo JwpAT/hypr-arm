26a94ae9cd073d007e91af3fec4db6e7608068ee
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-05-11 12:03:15 -0400

    Update firefox.sh

--- config/firefox/firefox.sh
+++ config/firefox/firefox.sh
@@ -3,14 +3,15 @@
 # Configuring Firefox for Open WebUI
 echo "✔️ Configuring Firefox profile for Open WebUI..."
 firefox -CreateProfile "openwebui /home/$USER/.mozilla/firefox/openwebui"
-cp -r ~hypr/config/Open-WebUI/chrome ~/.mozilla/firefox/openwebui/
-cp -r ~hypr/config/Open-WebUI/applications ~/.local/share/
+cp -r ~/hypr/config/Open-WebUI/chrome ~/.mozilla/firefox/openwebui/
+cp -r ~/hypr/config/Open-WebUI/applications ~/.local/share/
 echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> ~/.mozilla/firefox/openwebui/user.js
 
-# Configuring Main Firefox Plofile
+# Configuring Main Firefox Profile
+echo "✔️ Configuring main Firefox profile..."
 firefox -CreateProfile "default /home/$USER/.mozilla/firefox/default"
-cp -r ~hypr/config/firefox/chrome ~/.mozilla/firefox/default/
+cp -r ~/hypr/config/firefox/chrome ~/.mozilla/firefox/default/
 echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> ~/.mozilla/firefox/default/user.js
 
-echo "Firefox has been configured"
+echo "✅ Firefox has been configured"
 sleep 2
