15d5df8218fe4a756f7c132489d39fa10bacbcca
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-05-11 02:04:15 -0400

    Update open-webui.sh

--- config/Open-WebUI/open-webui.sh
+++ config/Open-WebUI/open-webui.sh
@@ -21,7 +21,7 @@ sleep 3
 
 # Installing Open WebUI
 echo "✔️ Installing Open WebUI..."
-docker run -d --network=host -v open-webui:/app/backend/data \
+sudo docker run -d --network=host -v open-webui:/app/backend/data \
   -e OLLAMA_BASE_URL=http://127.0.0.1:11434 \
   --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 
