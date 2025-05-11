3e65b23b719a951d2c48cb010503657c25cd31b3
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-05-11 11:43:28 -0400

    Add files via upload

---
+++ config/firefox/firefox.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Configuring Firefox for Open WebUI
+echo "✔️ Configuring Firefox profile for Open WebUI..."
+firefox -CreateProfile "openwebui /home/$USER/.mozilla/firefox/openwebui"
+cp -r ~hypr/config/Open-WebUI/chrome ~/.mozilla/firefox/openwebui/
+cp -r ~hypr/config/Open-WebUI/applications ~/.local/share/
+echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> ~/.mozilla/firefox/openwebui/user.js
+
+# Configuring Main Firefox Plofile
+firefox -CreateProfile "default /home/$USER/.mozilla/firefox/default"
+cp -r ~hypr/config/firefox/chrome ~/.mozilla/firefox/default/
+echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> ~/.mozilla/firefox/default/user.js
+
+echo "Firefox has been configured"
+sleep 2
