ad82fd3fb73cd26a0265e96f623260b9fdba936e
jwpat <jwpdaviesjr@gmail.com>
2025-05-10 23:08:43 -0400

    openwebui add

---
+++ config/Open-WebUI/open-webui.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Install Ollama
+curl -fsSL https://ollama.com/install.sh | sh
+
+#Installing and Configuring Docker
+sudo pacman -S docker
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
+
+#Installing Open WebUI
+docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+
+echo "your WebUI should be available at http://localhost:8080"
+
+#Configuring Firefox for Open Webui
+cp -r ~/hypr/Open-WebUI/openwebui ~/.mozilla/firefox/
+sudo cp ~/hypr/Open-WebUI/open-webui.desktop /usr/share/applications/
+
+# Define the profile section
+profile_section='
+[Profile3]
+Name=openwebui
+IsRelative=1
+Path=openwebui
+Default=1
+'
+
+# Path to the Firefox profiles.ini file
+profiles_ini="$HOME/.mozilla/firefox/profiles.ini"
+
+# Check if the section already exists
+if ! grep -q "\[Profile3\]" "$profiles_ini"; then
+    echo "$profile_section" >> "$profiles_ini"
+    echo "✔️ Profile3 added to profiles.ini"
+else
+    echo "⚠️ Profile3 already exists. No changes made."
+fi
+
+
+
+
