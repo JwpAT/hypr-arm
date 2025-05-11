68c58316b9c788f1f120850f247a7937c2e22bb2
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-05-11 17:05:06 -0400

    Update firefox.sh

--- config/firefox/firefox.sh
+++ config/firefox/firefox.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Copying profiles.ini to firefox 
+echo "copying profiles.ini"
+cp ~/hypr/config/firefox/profiles.ini ~/.mozilla/firefox/
+
 # Configuring Firefox for Open WebUI
-echo "✔️ Configuring Firefox profile for Open WebUI..."
+echo "Configuring Firefox profile for Open WebUI..."
 firefox -CreateProfile "openwebui /home/$USER/.mozilla/firefox/openwebui"
+mkdir -p ~/.mozilla/firefox/openwebui
 cp -r ~/hypr/config/Open-WebUI/chrome ~/.mozilla/firefox/openwebui/
 cp -r ~/hypr/config/Open-WebUI/applications ~/.local/share/
 echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> ~/.mozilla/firefox/openwebui/user.js
@@ -10,8 +15,9 @@ echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >
 # Configuring Main Firefox Profile
 echo "✔️ Configuring main Firefox profile..."
 firefox -CreateProfile "default /home/$USER/.mozilla/firefox/default"
+mkdir -p ~/.mozilla/firefox/default
 cp -r ~/hypr/config/firefox/chrome ~/.mozilla/firefox/default/
 echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> ~/.mozilla/firefox/default/user.js
 
-echo "✅ Firefox has been configured"
+echo "Firefox has been configured"
 sleep 2
