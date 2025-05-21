be075f7c6e92c27dadd72888a28639bbb2f2a272
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-05-21 09:40:36 -0400

    Update firefox.sh

--- config/firefox/firefox.sh
+++ config/firefox/firefox.sh
@@ -2,22 +2,21 @@
 
 # Copying profiles.ini to firefox 
 echo "copying profiles.ini"
-cp ~/hypr/config/firefox/profiles.ini ~/.mozilla/firefox/
-cp ~/hypr/config/firefox/installs.ini ~/.mozilla/firefox/
+cp ~./config/firefox/profiles.ini ~/.mozilla/firefox/
+cp ~./config/firefox/installs.ini ~/.mozilla/firefox/
 
 # Configuring Firefox for Open WebUI
 echo "Configuring Firefox profile for Open WebUI..."
 firefox -CreateProfile "ai /home/$USER/.mozilla/firefox/ai"
 mkdir -p ~/.mozilla/firefox/ai
-cp -r ~/hypr/config/Open-WebUI/chrome ~/.mozilla/firefox/ai/
-cp -r ~/hypr/config/Open-WebUI/applications ~/.local/share/
+cp -r ~./config/Open-WebUI/chrome ~/.mozilla/firefox/ai/
 echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> ~/.mozilla/firefox/ai/user.js
 
 # Configuring Main Firefox Profile
 echo "✔️ Configuring main Firefox profile..."
 firefox -CreateProfile "default /home/$USER/.mozilla/firefox/default"
 mkdir -p ~/.mozilla/firefox/default
-cp -r ~/hypr/config/firefox/chrome ~/.mozilla/firefox/default/
+cp -r ~./config/firefox/chrome ~/.mozilla/firefox/default/
 echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> ~/.mozilla/firefox/default/user.js
 
 echo "Firefox has been configured"
