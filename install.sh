eacb04281cea303ea55dbcd00586634df9d5fcfb
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-05-21 17:10:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ sudo cp -r ~/.config/wlogout/icons/ /usr/share/wlogout/
 echo "-----------------------------------------------------------------------------------------------------------"
 
 if command -v spicetify >/dev/null 2>&1; then
-  echo "Installation has completed! Please reboot your system. To install the Catppuccin Mocha theme for Spotify/Spicetify, locate ~/.config/scripts/spicetifyconf.sh and execute the script."
+  echo "Installation has completed! Please reboot your system. To install the Catppuccin Mocha theme for Spotify/Spicetify, locate ~/.config/scripts/spicetify.sh and execute the script."
 else
   echo "Installation has completed! Reboot your system."
 fi
